36c566038d6ab129f80df6d6794a30dd40539c71
Dmitry16 <europartner@inbox.ru>
2019-07-03 09:04:47 +0300

    caching_decorator

---
+++ decor.js
@@ -0,0 +1,32 @@
+// caching decorator.
+// let's say the function 'slow' produces a very slow (expensive) process.
+// but it's a pure function: with the same input (parameters) it returns the same result.
+// so in order to avoid expensive repeated calculations we can remember (cache) the results.
+
+function slow(x) {
+  // here are some expensive calculations
+  console.log('i am very slow :(', x);
+  return x;
+}
+
+function cachingDecorator(func) {
+  let cache = new Map();
+
+  return function(x) {
+    if (cache.has(x)) {
+      return x;
+    }
+    // if there is no 'x' in cache then calculate (original slow) 'x', save the result
+    // into cache and return the result
+    let result = func(x);
+    cache.set(x, result);
+    return result;
+  }
+}
+
+let decoratedSlow = cachingDecorator(slow);
+
+console.log('i am not cached :( ', decoratedSlow(1) );
+console.log('i was cached :) ', decoratedSlow(1) );
+console.log('i am not cached :( ',  decoratedSlow(2) );
+console.log('i was cached :) ',  decoratedSlow(2) );
