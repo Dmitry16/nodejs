f4256afce89fe8ff342855615c13c959e13d6245
Dmitry16 <europartner@inbox.ru>
2019-07-04 08:02:44 +0300

    spy_enhanced

--- pract/spy.js
+++ pract/spy.js
@@ -1,28 +1,26 @@
 function work(a, b) {
-  console.log('original work::', a + b ); // work is an arbitrary function or method
+  console.log('the sum in the original work function is:', a + b ); // work is an arbitrary function or method
 }
 
 work = spy(work);
-
 work(1, 2); // 3
 work(4, 5); // 9
+console.log('work.calls::', work.calls);
 
+let callNum = 1;
 for (let args of work.calls) {
-  console.log( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+  console.log( `arguments in call N${callNum}: work( ${args.join()} )` ); // "call:1,2", "call:4,5"
+  callNum++;
 }
 
 function spy(func) {
-
-  function wrapper(args) {
-    wrapper.calls.push(args);
-    return func.apply(this, arguments);
-  }
-
+  
   wrapper.calls = [];
+
+  function wrapper(...args) {
+    wrapper.calls.push(args);
+    return func.apply(work, arguments);
+  }
 
   return wrapper;
 }
-// work.calls = new Map([['key1', 'value1'], ['key2', 'value2']]);
-// work.calls.delete('key1');
-// work.calls.delete('key2');
-console.log('work.calls::', work.calls);
