85ac1bb56d6b16585d545dcc55a83b8060526eac
Dmitry16 <europartner@inbox.ru>
2019-07-09 07:31:57 +0300

    server

--- pract/regexp.js
+++ pract/regexp.js
@@ -1,10 +1,11 @@
 let regExp = /[a-d]abo/gi;
 // let regExp = new RegExp('l', 'g');
 let regExp2 = /\d+/;
+let regExp3 = /\.com$/;
 
 // console.log(regExp.test('zhabo'));
 // console.log(regExp.test('DAabo'));
 
 console.log(regExp2.test('7'));
-// console.log(regExp2.test('888'));
+console.log(regExp3.test('aaa@zzz.com'));
 // console.log(regExp2.test('888'));
---
+++ pract/server.js
@@ -0,0 +1,13 @@
+let http = require('http');
+let server = new http.Server();
+
+server.listen(1337, '127.0.0.1');
+console.log('Server running at 127.0.0.1:1337')
+
+let count = 0;
+
+server.on('request', function(req, res) {
+  res.end('Count: ' + ++count);
+});
+
+let emit = server.emit;
