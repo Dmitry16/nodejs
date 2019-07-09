6b20e6222a5d314014e7343a9af7845870eb3ff4
Dmitry16 <europartner@inbox.ru>
2019-07-09 08:17:11 +0300

    server.emit

--- pract/server.js
+++ pract/server.js
@@ -6,8 +6,13 @@ console.log('Server running at 127.0.0.1:1337')
 
 let count = 0;
 
+let emit = server.emit;
+server.emit = function(event) {
+  console.log(event);
+  emit.apply(server, arguments);
+}
+
 server.on('request', function(req, res) {
   res.end('Count: ' + ++count);
 });
 
-let emit = server.emit;
