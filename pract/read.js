e64ec4955825691e66c92b4418b24fc7f2b08cf0
Dmitry16 <europartner@inbox.ru>
2019-07-15 09:51:41 +0300

    read3

--- pract/read.js
+++ pract/read.js
@@ -1,8 +1,11 @@
 let fs = require('fs');
 
-fs.readFile(__filename, function(err, data){
-  if (err) {
-    console.error(err)
+fs.readFile('zzz', function(err, data){
+  if (err && err.code === 'ENOENT') {
+    let subStr = err.message.substring(err.message.search(',')+1);
+    let errStr = err.message.replace(subStr, '');
+    console.error(errStr.replace('ENOENT', `Error when${subStr}`).replace(',',''));
   }
-  console.log(data.toString())
-})
+  if (data)
+    console.log(data.toString());
+});
