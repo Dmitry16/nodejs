6d14d03f54ea9dbc1914251f2a011e4165d6625e
Dmitry16 <europartner@inbox.ru>
2019-07-16 09:42:22 +0300

    write_documented

--- pract/pipe.js
+++ pract/pipe.js
@@ -13,7 +13,26 @@ console.log('server is listening on port 1313')
 function sendFile(file, res) {
   
   file.on('readable', write)
-  
+
+  /*
+  this is the 'bad' version of the function write because it writes all the big data
+  the res obj. so if the client has slow connection we can face the RAM saturation
+  holding all the big data in the res obj to be consumed
+    
+  function write() {
+    let fileContent = file.read();
+    console.log('zzz::', fileContent.length);
+    if (fileContent)
+      res.write(fileContent);
+  }
+  */
+
+  /*
+  this is the good one :)
+  by removeListener command we stop writing to the buffer of the res object
+  if the data is not consumed by client (e.g. when connection is very slow)
+  this way we prevent the big file data been hang in the RAM 
+  */
   function write() {
     let fileContent = file.read();
     if (fileContent)
